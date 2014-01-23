c897fe4326a0d2d1b47f92f15baafd7d258f5920
Vadim Lobanov <vadim@lobanov.pw>
2014-01-23 13:13:19 +0700

    satisfy specs for ar_settings

--- spec/dummy/app/models/ar_settings.rb
+++ spec/dummy/app/models/ar_settings.rb
@@ -1,9 +1,12 @@
 class ArSettings < ActiveRecord::Base
-  def set_value(name, value)
-
+  def self.set_value(name, value)
+    setting = ArSettings.find_or_initialize_by(name: name.to_s)
+    setting.value = Marshal.dump(value)
+    setting.save!
   end
 
-  def get_value(name)
-    
+  def self.get_value(name)
+    value = ArSettings.find_by(name: name.to_s).value
+    Marshal.load(value)
   end
 end
