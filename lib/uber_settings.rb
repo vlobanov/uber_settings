8fb989ed7711de0a9d1cdf1b0f56d0092a2a2f84
Vadim Lobanov <vadim@lobanov.pw>
2014-01-25 15:12:00 +0700

    require dsl and exceptions

--- lib/uber_settings.rb
+++ lib/uber_settings.rb
@@ -3,4 +3,6 @@ module UberSettings
   require 'uber_settings/data_providers/mongoid'
   require 'uber_settings/customizable_file'
   require 'uber_settings/settings'
+  require 'uber_settings/defaults_dsl'
+  require 'uber_settings/exceptions'
 end
