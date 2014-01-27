78b4f22103e4af2848a43fc0b7c4b274f105142e
Vadim Lobanov <vadim@lobanov.pw>
2014-01-27 18:35:47 +0700

    move types logic to special file

--- lib/uber_settings.rb
+++ lib/uber_settings.rb
@@ -1,11 +1,9 @@
 module UberSettings
-  DATA_TYPES = [:string, :text, :integer, :float, :boolean, :file]
-
-
+  require 'uber_settings/types'
+  require 'uber_settings/exceptions'
   require 'uber_settings/data_providers/active_record'
   require 'uber_settings/data_providers/mongoid'
   require 'uber_settings/customizable_file'
   require 'uber_settings/settings'
   require 'uber_settings/defaults_dsl'
-  require 'uber_settings/exceptions'
 end
---
+++ lib/uber_settings/types.rb
@@ -0,0 +1,3 @@
+module UberSettings
+  DATA_TYPES = [:string, :text, :integer, :float, :boolean, :file]
+end
