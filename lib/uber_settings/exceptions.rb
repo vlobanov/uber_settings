c2483c013809ac3a57679a2451cf0db8e66da8fa
Vadim Lobanov <vadim@lobanov.pw>
2014-01-27 17:50:03 +0700

    add fields definition specifin exceptions

--- lib/uber_settings/exceptions.rb
+++ lib/uber_settings/exceptions.rb
@@ -2,4 +2,12 @@ module UberSettings
   class SettingNotFound < StandardError
 
   end
+
+  class InvalidSettingName < StandardError
+
+  end
+
+  class InvalidSettingOptions < StandardError
+    
+  end
 end
