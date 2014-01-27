e3a43066083e199ccd1fe98d8b7fe4317d5d08c1
Vadim Lobanov <vadim@lobanov.pw>
2014-01-27 17:49:31 +0700

    move constant definition to top, before requires

--- lib/uber_settings.rb
+++ lib/uber_settings.rb
@@ -1,10 +1,11 @@
 module UberSettings
+  DATA_TYPES = [:string, :text, :integer, :float, :boolean, :file]
+
+
   require 'uber_settings/data_providers/active_record'
   require 'uber_settings/data_providers/mongoid'
   require 'uber_settings/customizable_file'
   require 'uber_settings/settings'
   require 'uber_settings/defaults_dsl'
   require 'uber_settings/exceptions'
-
-  DATA_TYPES = [:string, :text, :integer, :float, :boolean, :file]
 end
