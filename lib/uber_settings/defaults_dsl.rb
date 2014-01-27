0c225220048ea6784f1d2bbfeebd2bffe5a149ed
Vadim Lobanov <vadim@lobanov.pw>
2014-01-27 17:50:40 +0700

    defaults dsl implementation + specs

--- lib/uber_settings/defaults_dsl.rb
+++ lib/uber_settings/defaults_dsl.rb
@@ -1,11 +1,38 @@
 module UberSettings
   class DefaultsDSL
-    def initialize(data_provider)
+    def initialize(data_provider, &block)
       @data_provider = data_provider
+      instance_eval(&block) if block
     end
 
     def field(name, default_value, options = {})
-      data_provider.create_setting_with_default(name, default_value, options)
+      check_name!(name)
+      check_options!(options)
+      @data_provider.create_setting_with_default(name, default_value, options)
     end
+
+    UberSettings::DATA_TYPES.each do |type|
+      define_method(type) do |name, value, options = {}|
+        options[:type] = type
+        field(name, value, options)
+      end
+    end
+
+    private
+      def check_name!(name)
+        unless name.kind_of?(String) || name.kind_of?(Symbol)
+          raise InvalidSettingName, "Name must be String or Symbol"
+        end
+      end
+
+      def check_options!(options)
+        raise InvalidSettingOptions, "Options must be a Hash" unless options.kind_of? Hash
+
+        allowed_options = [:type, :description]
+        redundant_options = options.values - allowed_options
+        unless redundant_options.empty?
+          raise InvalidSettingOptions, "Unexpected options #{redundant_options.join(", ")}"
+        end
+      end
   end
 end
--- spec/models/defaults_dsl_spec.rb
+++ spec/models/defaults_dsl_spec.rb
@@ -7,4 +7,57 @@ describe UberSettings::DefaultsDSL do
   end
 
   specify { @defaults_dsl.should respond_to :field }
+
+  it "yields block if passed to constructor" do
+    UberSettings::DefaultsDSL.any_instance.expects(:some_instance_method)
+    UberSettings::DefaultsDSL.new(@data_provider) do
+      some_instance_method
+    end
+  end
+
+  describe "#field" do
+    it "passes name and default value to data_provider's create_setting_with_default, options are empty by default" do
+      name = :some_name
+      value = 42
+      @data_provider.expects(:create_setting_with_default).with(name, value, {})
+      @defaults_dsl.field(name, value)
+    end
+
+    it "throws exception if name is not string or symbol" do
+      name = 123
+      expect { @defaults_dsl.field(name, "some value") }.to raise_error(UberSettings::InvalidSettingName)
+    end
+
+    it "throws exception if unexpected options are given" do
+      expect { @defaults_dsl.field(:ok, :ok_too, weird: "something") }.to raise_error(UberSettings::InvalidSettingOptions)
+    end
+
+    it "throws exception if given options is not hash" do
+      expect { @defaults_dsl.field(:ok, :ok_too, [:type]) }.to raise_error(UberSettings::InvalidSettingOptions)
+    end
+  end
+
+  describe "field with type aliases" do
+    UberSettings::DATA_TYPES.each do |type|
+      specify { @defaults_dsl.should respond_to type }
+    end
+
+    def self.it_calls_field_with_type(type, value)
+      it "##{type.to_s} calls #field with {type: #{type.inspect}}" do
+        name = rand(0..1000).to_s
+        @defaults_dsl.expects(:field).with(name, value, {type: type})
+        @defaults_dsl.send(type, name, value)
+      end
+    end
+
+    # DATA_TYPES = [:string, :text, :integer, :float, :boolean, :file]
+
+    it_calls_field_with_type :string, "Some string"
+    it_calls_field_with_type :text, "Some string, but as text"
+    it_calls_field_with_type :integer, 42
+    it_calls_field_with_type :float, 123.55
+    it_calls_field_with_type :boolean, 123.55
+  end
+
+
 end
