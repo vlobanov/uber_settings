c50ed95993d2e53589cffef6fbe7c3117133e1b9
Vadim Lobanov <vadim@lobanov.pw>
2014-01-27 15:23:04 +0700

    make DefaultsDSL a class

--- lib/uber_settings/defaults_dsl.rb
+++ lib/uber_settings/defaults_dsl.rb
@@ -1,5 +1,9 @@
 module UberSettings
-  module DefaultsDSL
+  class DefaultsDSL
+    def initialize(data_provider)
+      @data_provider = data_provider
+    end
+
     def field(name, default_value, options = {})
       data_provider.create_setting_with_default(name, default_value, options)
     end
--- spec/models/defaults_dsl_spec.rb
+++ spec/models/defaults_dsl_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe UberSettings::DefaultsDSL do
-  
+  before(:each) do
+    @data_provider = mock()
+    @defaults_dsl = UberSettings::DefaultsDSL.new(@data_provider)
+  end
+
+  specify { @defaults_dsl.should respond_to :field }
 end
